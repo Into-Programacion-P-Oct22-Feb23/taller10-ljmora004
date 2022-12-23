6ccbb2b0864bc2f9e70d05ed620fa802270611e3
Luis Mora <ljmora1@utpl.edu.ec>
2022-12-23 16:58:50 -0500

    Taller 10

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -1,8 +1,32 @@
-
 package paquete5;
 
 public class Problema05 {
+
     public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        char [][] inicial = new char[4][2];
+        String [][] inicial2 = new String[4][2];
+        String mensaje = "";
         
+        for(int fila = 0; fila < estudiantes.length; fila++){
+            for(int columna = 0; columna < estudiantes[fila].length; columna++){
+                inicial[fila][columna] = estudiantes[fila][columna].charAt(0);
+                inicial2[fila][columna] = String.valueOf(inicial[fila][columna]);
+            }
+        }
+        for(int fila = 0; fila < estudiantes.length; fila++){
+            for(int columna = 0; columna < estudiantes[fila].length; columna++){
+                if(inicial2[fila][columna].equals("S") || 
+                        inicial2[fila][columna].equals("P") || 
+                        inicial2[fila][columna].equals("T")){
+                    mensaje = String.format("%s%s\n", mensaje, estudiantes[fila][columna]);
+                }
+            }
+        }
+        System.out.println(mensaje);
     }
 }
--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete6;
 
-/**
- *
- * @author reroes
- */
 public class Problema06 {
-    
+
+    public static void main(String[] args) {
+
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String mensaje = "";
+        int[][] caracter = new int[4][2];
+        
+        for(int fila = 0; fila < estudiantes.length; fila++){
+            for(int columna = 0; columna < estudiantes[fila].length; columna++){
+                caracter[fila][columna] = estudiantes[fila][columna].length(); 
+            }
+        }
+        for(int fila = 0; fila < estudiantes.length; fila++){
+            for(int columna = 0; columna < estudiantes[fila].length; columna++){
+                if(caracter[fila][columna] == 11){
+                    mensaje = String.format("%s%s\n", mensaje, 
+                            estudiantes[fila][columna]);
+                }
+            }
+        }
+        System.out.println(mensaje);
+    }
 }
